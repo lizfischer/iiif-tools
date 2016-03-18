4e59ed24568bc2039f61e8b3b73ade09cf7b2535
lizfischer <efisch17@stanford.edu>
2016-03-18 06:41:56 -0700

    commenting

--- cropper/js/cropper.js
+++ cropper/js/cropper.js
@@ -2,19 +2,28 @@ $(document).ready(function(){
 
 	var jcrop_api = null;
 
+	/* Trigger URL submit with ENTER */
 	$("#URL").keyup(function(event){
 		if(event.keyCode == 13){
 			$("#submit").click();
 		}
 	});
 
+	/* When URL submitted... */
 	$('#submit').click(function(){
+		// If there was an instance of jcrop, start fresh it
 		if (jcrop_api != null){
 			jcrop_api.destroy();
 		}
+		// Remove img styling-- clears out the 'display: none' when first image is loaded.
+		// This is also a workaround for a jcrop issue whereby it does not properly handle changing images and box constraints.
 		$('#target').removeAttr('style');
+
+		// Get input
 		var url = document.getElementById("URL").value;
+		// Display image
 		$('#target').attr("src", url);
+		// Start jcrop
 		$('#target').Jcrop({
 			boxHeight:700,
 			onSelect: getURL,
@@ -27,62 +36,64 @@ $(document).ready(function(){
 		});
 	});
 
-		function clearURL(){
-			$('#output').val("");
+
+	function clearURL(){
+		$('#output').val("");
+	}
+
+	/* Get the new URL from JCrop coordinates */
+	function getURL(c){
+		var oldURL = $('#target').attr('src');
+		var split = oldURL.split('/');
+		var sc = scaleCoords(c, oldURL); // scaled coordinates
+		var tc = translateCoords(sc, oldURL);
+
+		var coordStr = tc.x.toString() + ","+ tc.y.toString()+","+tc.w.toString()+","+tc.h.toString();
+		split[split.length - 4] = coordStr;
+		var newURL = split.join("/");
+		$('#output').val(newURL);
+	}
+
+	/* Adjusts for IIIF image scaling (pct:40, for example) */
+	function scaleCoords(c, url){
+		var x = c.x;
+		var y = c.y;
+		var w = c.w;
+		var h = c.h;
+		var split = url.split('/');
+		var pctStr = split[split.length-3];
+		if (pctStr != "full"){
+			var pct = pctStr.split(':')[1];
+			var scale = 100/pct;
+			x *= scale;
+			y *= scale;
+			w *= scale;
+			h *= scale;
 		}
-	// Get the new URL from JCrop coordinates
-		function getURL(c){
-			var oldURL = $('#target').attr('src');
-			var split = oldURL.split('/');
-			var sc = scaleCoords(c, oldURL); // scaled coordinates
-			var tc = translateCoords(sc, oldURL);
+		x = Math.round(x);
+		y = Math.round(y);
+		w = Math.round(w);
+		h = Math.round(h);
 
-			var coordStr = tc.x.toString() + ","+ tc.y.toString()+","+tc.w.toString()+","+tc.h.toString();
-			split[split.length - 4] = coordStr;
-			var newURL = split.join("/");
-			$('#output').val(newURL);
+		return {'x':x, 'y':y, 'w':w, 'h':h}
+	}
+
+	/* Moves box over/down already cropped URLs. Takes scaled coordinates */
+	function translateCoords(sc, oldURL){
+		var x = parseInt(sc.x);
+		var y = parseInt(sc.y);
+		var w = parseInt(sc.w);
+		var h = parseInt(sc.h);
+
+		var oldCoords = oldURL.split('/')[oldURL.split('/').length - 4]
+
+		if (oldCoords != "full"){
+			var split = oldCoords.split(',');
+			x += parseInt(split[0]);
+			y += parseInt(split[1]);
 		}
-
-		// Adjusts for IIIF image scaling (pct:40, for example)
-		function scaleCoords(c, url){
-			var x = c.x;
-			var y = c.y;
-			var w = c.w;
-			var h = c.h;
-			var split = url.split('/');
-			var pctStr = split[split.length-3];
-			if (pctStr != "full"){
-				var pct = pctStr.split(':')[1];
-				var scale = 100/pct;
-				x *= scale;
-				y *= scale;
-				w *= scale;
-				h *= scale;
-			}
-			x = Math.round(x);
-			y = Math.round(y);
-			w = Math.round(w);
-			h = Math.round(h);
-
-			return {'x':x, 'y':y, 'w':w, 'h':h}
-		}
-
-		// Takes scaled coordinates
-		function translateCoords(sc, oldURL){
-			var x = parseInt(sc.x);
-			var y = parseInt(sc.y);
-			var w = parseInt(sc.w);
-			var h = parseInt(sc.h);
-
-			var oldCoords = oldURL.split('/')[oldURL.split('/').length - 4]
-
-			if (oldCoords != "full"){
-				var split = oldCoords.split(',');
-				x += parseInt(split[0]);
-				y += parseInt(split[1]);
-			}
-			return {'x':x, 'y':y, 'w':w, 'h':h}
-		}
+		return {'x':x, 'y':y, 'w':w, 'h':h}
+	}
 
 
 });
